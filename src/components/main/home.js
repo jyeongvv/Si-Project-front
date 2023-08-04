8446e22022a5f3bcdd99621214c1424d5328457b
jyeongvv <kjy812467@gmail.com>
2023-08-04 14:00:47 +0900

    feat:update

--- src/components/main/home.js
+++ src/components/main/home.js
@@ -3,14 +3,12 @@ import './home.css';
 import SearchBar from '../../search/Search';
 import Upload from '../upload/PictureUpload';
 import Info from '../footer/Info';
-import SpeedDialTooltipOpen from '../menu/Menu';
 
 const Home = () => {
     return (
         <div>
             <SearchBar />
             <Upload />
-            <SpeedDialTooltipOpen />
             <Info />
         </div>
     )
