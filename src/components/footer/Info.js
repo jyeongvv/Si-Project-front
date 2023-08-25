33b65893fd9a49e6108261bdc48e8ba339fc4f45
jyeongvv <kjy812467@gmail.com>
2023-08-25 11:07:13 +0900

    feat:team profile

--- src/components/footer/Info.js
+++ src/components/footer/Info.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Info.css';
+import { Link } from 'react-router-dom';
 
 const Info = () => {
   return (
     <div className="footer-container">
       <div className="footer-section1">
-        <h2>팀 정보</h2>
-        <p>팀 정보</p>
+        <Link to="/team" className="button-link">
+          Crew Profile
+        </Link>  
       </div>
     </div>
   );
